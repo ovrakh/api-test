d4e3c466933de1aa093d9f0409641b1a1242f3eb
ovrakh <notik-lexa@mail.ru>
2018-01-15 17:30:25 +0300

    Add Promise.resolve

--- api/account.js
+++ api/account.js
@@ -4,19 +4,23 @@ const config = require('../config');
 const User = require('./models/user');
 
 router.get('/account', (req, res) => {
-  if (!req.headers['x-api-key']) { return res.send('Unauthorized')}
-  
-    let _username = jwt.decode(req.headers['x-api-key'], config.secretkey).username;
-    User.findOne({username: _username})
-      .then(user => {
-        if (!user) {
-          return Promise.reject('Unauthorized');
-        }
-        
-        return 'Authorized!';
-      })
-      .then(data => res.send({ success: true, data }))
-      .catch(err => res.send({ success: false, err }))
+  Promise.resolve()
+    .then(() => {
+      if (!req.headers['x-api-key']) return Promise.reject('Unauthorized');
+    })
+    .then(() => {
+      let username = jwt.decode(req.headers['x-api-key'], config.secretkey).username;
+      return User.findOne({ username })
+        .then(user => {
+          if (!user) {
+            return Promise.reject('Unauthorized');
+          }
+
+          return 'Authorized!';
+        })
+    })
+    .then(data => res.send({ success: true, data }))
+    .catch(err => res.send({ success: false, err }))
 });
 
 module.exports = router;
--- api/login.js
+++ api/login.js
@@ -4,34 +4,35 @@ const config = require('../config');
 const User = require('./models/user');
 const bcrypt = require('bcrypt');
 
-router.post ('/login', (req, res) => {
-    let p = Promise.resolve({then : function(onFulfill, onReject) {
-            if (!req.body.username || !req.body.password) {
-                onReject('Specify username!');
-            }
-        }
-        });
+router.post('/login', (req, res) => {
+  let p = Promise.resolve({
+    then: function (onFulfill, onReject) {
+      if (!req.body.username || !req.body.password) {
+        onReject('Specify username!');
+      }
+    }
+  });
 
-    p.then(User.findOne({ username: req.body.username })
-        .then(user => {
-            if (!user) {
-                return Promise.reject('Not found user')
-            }
+  p.then(User.findOne({username: req.body.username})
+    .then(user => {
+      if (!user) {
+        return Promise.reject('Not found user')
+      }
 
-            console.log(user.password);
-            console.log(bcrypt.compareSync(req.body.password, user.password));
+      console.log(user.password);
+      console.log(bcrypt.compareSync(req.body.password, user.password));
 
-            if (!bcrypt.compareSync(req.body.password, user.password)) {
-                return Promise.reject('Not found token')
-            }
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        return Promise.reject('Not found token')
+      }
 
-            let username = req.body.username;
-            return jwt.encode({ username }, config.secretkey);
-        })
-        .then(data => res.send({ success: true, data }))
-        .catch(err => res.send({ success: false, err }))
-        )
-    .catch(err => res.send({ success: false, err }))
+      let username = req.body.username;
+      return jwt.encode({username}, config.secretkey);
+    })
+    .then(data => res.send({success: true, data}))
+    .catch(err => res.send({success: false, err}))
+  )
+    .catch(err => res.send({success: false, err}))
 });
 
 module.exports = router;
